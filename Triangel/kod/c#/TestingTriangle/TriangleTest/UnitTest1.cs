89a6bd0c458e34aa536ad456492cbb27fdf5957c
Christfoffer Holmgren <christoffer.holmgren@gmail.com>
2013-11-25 20:32:38 +0100

    More or less the startpoint, with some small parts of code

---
+++ Triangel/kod/c#/TestingTriangle/TriangleTest/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        //Scalene: triangle med tre olika sidor
+        //Equilateral: liksidig triangel
+        //Isosceles: likbent triangel
+        [TestMethod]
+        public void isScaleneTest()
+        {
+            Assert.IsTrue(new Triangle(1.0, 2.0, 3.0).isScalene());
+            Assert.IsTrue(new Triangle(1.5, 1.6, 1.7).isScalene());
+
+            Assert.IsFalse(new Triangle(1.0, 1.0, 1.0).isScalene());
+            Assert.IsFalse(new Triangle(1.5, 1.5, 1.5).isScalene());
+        }
+    }
+}
