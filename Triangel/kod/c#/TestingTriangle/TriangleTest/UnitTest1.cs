be5dd2311f8a0c0ebe1541b8dd26b3296b4a0ec7
Christfoffer Holmgren <christoffer.holmgren@gmail.com>
2013-11-25 20:33:17 +0100

    Testing equilateral

--- Triangel/kod/c#/TestingTriangle/TriangleTest/UnitTest1.cs
+++ Triangel/kod/c#/TestingTriangle/TriangleTest/UnitTest1.cs
@@ -18,5 +18,19 @@ namespace UnitTestProject1
             Assert.IsFalse(new Triangle(1.0, 1.0, 1.0).isScalene());
             Assert.IsFalse(new Triangle(1.5, 1.5, 1.5).isScalene());
         }
+
+        //If it is Equilateral all sides are the same
+        //So we would expect a Triangle with all sides the same to return true
+        [TestMethod]
+        public void isEquilateralTest()
+        {
+            //These are expected to Assert to true
+            Assert.IsTrue(new Triangle(1.0, 1.0, 1.0).isEquilateral());
+            Assert.IsTrue(new Triangle(1.5, 1.5, 1.5).isEquilateral());
+
+            //These shouldn't assert
+            Assert.IsFalse(new Triangle(1.0, 2.0, 3.0).isEquilateral());
+            Assert.IsFalse(new Triangle(1.5, 1.6, 1.7).isEquilateral());
+        }
     }
 }
